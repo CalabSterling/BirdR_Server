dcbbb2e5f848789a4d2dd924ec5c08830a92296d
MatthewWLaMar <84155160+MatthewWLaMar@users.noreply.github.com>
2021-09-02 17:45:57 -0400

    prep for first merge

--- Controllers/sightingcontroller.js
+++ Controllers/sightingcontroller.js
@@ -2,8 +2,30 @@ let express = require('express');
 let router = express.Router();
 let bird = require('../db').import('../models/sighting');
 
-router.get('/sighting', function (req, res)
-{
-    res.send('Practice works')
+/**************************
+ **** VIEW ALL UPLOADS ****
+ **************************/
+
+router.get("/", (req, res) => {
+    let userid = req.user.id;
+    bird.findAll({
+      where: { owner_id: userid },
+    })
+      .then((sighting) => res.status(200).json(sighting))
+      .catch((err) => res.status(500).json({ error: err }));
+  });
+
+/***********************
+ **** DELETE UPLOAD ****
+ **********************/
+
+router.delete('/sighting/:id', function (req, res) {
+    const query = {where: {/* this needs code */}}
+
+    bird.destroy(query)
+    .then(() => res.status(200).json({message: "Upload has been removed"}))
+    .catch((err) => res.status(500).json({error: err}))
 })
+
+
 module.exports = router
