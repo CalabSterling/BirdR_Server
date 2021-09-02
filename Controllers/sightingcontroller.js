89757a9675a2800cfbc6398ae2249a5a0f3b6a4f
Kara Smith <MsKaraBSmith@gmail.com>
2021-09-02 17:53:01 -0400

    adding jwt, sighting, and mine

--- Controllers/sightingcontroller.js
+++ Controllers/sightingcontroller.js
@@ -1,9 +1,33 @@
 let express = require('express');
 let router = express.Router();
-let bird = require('../db').import('../models/sighting');
+let Sighting = require('../db').import('../models/sighting');
 
 router.get('/sighting', function (req, res)
 {
     res.send('Practice works')
 })
+
+router.post('/sighting', validateSession, (req, res) => {
+    const sightingEntry = {
+        bird: req.body.sighting.bird,
+        location: req.body.sighting.location,
+        description: req.body.sighting.description,
+        image: req.body.sighting.image,
+        rarity: req.body.sighting.rarity,
+        owner_id: req.user.id
+    }
+    Sighting.create(sightingEntry)
+    .then(sighting => res.status(200).json(sighting))
+    .catch(err => res.status(500).json({ error: err }))
+})
+
+router.get('/mine', validateSession, (req, res) => {
+    let userid = req.user.id
+    Sighting.findAll({
+        where: { owner_id: userid }
+    })
+    .then(log => res.status(200).json(log))
+    .catch(err => res.status(500).json({ error: err }))
+})
+
 module.exports = router
