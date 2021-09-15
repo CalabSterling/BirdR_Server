33203c4d15532467716b08655d7491e44915ddbd
MatthewWLaMar <84155160+MatthewWLaMar@users.noreply.github.com>
2021-09-14 19:33:51 -0500

    pulling updated server

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
-let express = require('express')
+let express = require("express");
 let app = express();
-let sequelize = require('./db');
+let sequelize = require("./db");
 
-let user = require('./Controllers/usercontroller');
-let sighting = require('./Controllers/sightingcontroller')
+
+
+let user = require("./Controllers/usercontroller");
+let sighting = require("./Controllers/sightingcontroller");
 
 sequelize.sync();
+app.use(require('./middleware/headers'));
+
 app.use(express.json());
-app.use('/sighting', sighting);
-app.use('/user', user);
+app.use("/sighting", sighting);
+app.use("/user", user);
 
 app.listen(3000, () => {
-    console.log("app is listening");
-})
+  console.log("app is listening");
+});
